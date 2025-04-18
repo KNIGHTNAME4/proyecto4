fa68c0a2fe483b89db624b86bc3b287a3b121958
KNIGHTNAME4 <francodaniel.knight@gmail.com>
2025-04-18 17:09:43 -0500

    Update botones4.js

--- botones4.js
+++ botones4.js
@@ -1,11 +1,22 @@
-alert("bienvenido");
-
 let boton = document.getElementById("p2");
-
+let boton2= document.getElementById("okey0");
+let boton3= document.getElementById("okey1");
 function botones(){
-    alert("quien eres?");
+ 
+    document.getElementById("bancomalvado").style.display="flex";
+   
+}
 
+function botones2(){
+document.getElementById("bancomalvado").style.display="none";
+ document.getElementById("bonito").style.display="flex";
+}
+
+function botones3(){
+document.getElementById("bonito").style.display="none";
+ 
 }
 
 boton.onclick = botones;
-
+boton2.onclick =botones2;
+boton3.onclick =botones3;
